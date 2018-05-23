e673ef97025234bff93f966571f7c876bd6b88a2
gersteinj <gersteinj@gmail.com>
2018-05-23 10:55:28 -0400

    index page works

--- rostertools/urls.py
+++ rostertools/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('view/<int:roster_id>/', views.view_roster, name='view roster'),
 ]
--- rostertools/views.py
+++ rostertools/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Roster, Unit
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is where the roster tools will be")
+    all_rosters = Roster.objects.all()
+    template = loader.get_template('rostertools/index.html')
+    context = {'all_rosters': all_rosters}
+    return HttpResponse(template.render(context, request))
+
+def view_roster(request, roster_id):
+    return HttpResponse(f"You're viewing roster {roster_id}.")
