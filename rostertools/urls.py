f4eb3ada5926a35292633fb85958387ada6da01f
gersteinj <gersteinj@gmail.com>
2018-05-24 10:26:53 -0400

    reorganized URLs

--- rostertools/urls.py
+++ rostertools/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name='rostertools'
 urlpatterns = [
     path('', views.index, name='index'),
     path('view/<int:roster_id>/', views.view_roster, name='view roster'),
