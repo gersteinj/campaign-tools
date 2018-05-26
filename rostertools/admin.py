e2ad6fe5cc6b67c2177ae59fa3f1f6fec72cf20e
gersteinj <gersteinj@gmail.com>
2018-05-26 14:54:42 -0400

    modified admin site for users

--- rostertools/admin.py
+++ rostertools/admin.py
@@ -3,5 +3,15 @@ from .models import Roster, Unit
 
 # Register your models here.
 
-admin.site.register(Roster)
-admin.site.register(Unit)
+# admin.site.register(Roster)
+# admin.site.register(Unit)
+
+class UnitUnline(admin.StackedInline):
+    model = Unit
+    extra = 0
+
+class RosterAdmin(admin.ModelAdmin):
+    fields = ['owner', 'title']
+    inlines = [UnitUnline]
+
+admin.site.register(Roster, RosterAdmin)
