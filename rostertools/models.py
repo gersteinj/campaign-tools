fbd5a3dd2518f2e93a02c83fe26ed1a39a18a504
gersteinj <gersteinj@gmail.com>
2018-05-27 10:30:06 -0400

    updated models to allow for ownership

--- rostertools/models.py
+++ rostertools/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Roster(models.Model):
     title = models.CharField(max_length=100)
-    owner = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return f"{self.title}: {self.owner}"
