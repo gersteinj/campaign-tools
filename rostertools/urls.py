6efe0fdd606b909f0762d3526f2b4ef814ca8485
gersteinj <gersteinj@gmail.com>
2018-05-28 09:21:52 -0400

    general template improvements

--- rostertools/urls.py
+++ rostertools/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name='rostertools'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('view-user/<str:username>/', views.user_rosters, name='user rosters'),
+    path('user/<str:username>/', views.user_rosters, name='user'),
     path('view/<int:roster_id>/', views.view_roster, name='view roster'),
 ]
